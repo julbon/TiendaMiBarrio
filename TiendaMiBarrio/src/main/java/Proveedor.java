4151e2650d3f2356b079f209b02ded962c9b78b9
acamacho <acamacho@LptAcamacho.domicro>
2021-09-26 17:34:54 -0500

    C7: Se agrega clase proveedor

---
+++ TiendaMiBarrio/src/main/java/Proveedor.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author acamacho
+ */
+public class Proveedor {
+    //Attributos
+    
+    public int idProv;
+    public String nombreProv;
+    public String dirProv;
+    public int telProv;
+    public String EmialProv;
+    
+    //Constructor
+
+    public Proveedor() {
+    }
+    // metodos get setter
+    
+    public int getIdProv() {
+        return idProv;
+    }
+
+    public void setIdProv(int idProv) {
+        this.idProv = idProv;
+    }
+
+    public String getNombreProv() {
+        return nombreProv;
+    }
+
+    public void setNombreProv(String nombreProv) {
+        this.nombreProv = nombreProv;
+    }
+
+    public String getDirProv() {
+        return dirProv;
+    }
+
+    public void setDirProv(String dirProv) {
+        this.dirProv = dirProv;
+    }
+
+    public int getTelProv() {
+        return telProv;
+    }
+
+    public void setTelProv(int telProv) {
+        this.telProv = telProv;
+    }
+
+    public String getEmialProv() {
+        return EmialProv;
+    }
+
+    public void setEmialProv(String EmialProv) {
+        this.EmialProv = EmialProv;
+    }
+    
+    public void crearProveedor (){
+        
+    }
+    
+        
+    
+}
